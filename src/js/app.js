cbc8b77a723174da6ac044e9db11a353b10c7968
Juan Manuel <jmmaronas@gmail.com>
2022-03-18 14:57:58 -0300

    Agregando modal y cursos

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,16 @@
-import  cursos  from "./db.js";
+import cursos from "./db.js";
 
-console.log(cursos)
-const cursosContainer= document.querySelector("#cursosContainer");
+document.addEventListener("DOMContentLoaded", () => {
+  renderCursos();
+});
 
-for(const curso of cursos){
-    cursosContainer.innerHTML+=`
-            <div class="card mb-3" >
+const cursosContainer = document.querySelector("#cursosContainer");
+
+function renderCursos() {
+  for (const curso of cursos) {
+    curso.innerHTML = "";
+    cursosContainer.innerHTML += `
+            <div class="card mb-3 py-2" >
                 <div class="row g-0">
                   <div class="col-md-4">
                     <img src="${curso.img}" class="img-fluid rounded-start" alt="...">
@@ -14,11 +19,60 @@ for(const curso of cursos){
                     <div class="card-body">
                       <h5 class="card-title">${curso.title}</h5>
                       <p class="card-text">${curso.description}</p>
+                      <p class="card-text">Duracion: ${curso.duration} semanas</p>
+                      <p class="card-text">Modalidad: ${curso.modality}</p>
                       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
-                  </div>
-                </div>
-            </div>
-              `    
+            
+                    <!-- Button trigger modal -->
+                    <div class="d-flex">
+                        <button type="button" class="btn btn-success d-flex px-lg-4 mx-2" data-bs-toggle="modal" data-bs-target="#exampleModal${curso.id}">
+                        + Info
+                        </button>
+                        <button type="button" class="btn btn-primary px-lg-4 mx-2">comprar</button>
+                        <button type="button" class="btn btn-secondary px-lg-4 mx-2">Agregar al carrito</button>
+                    </div>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal${curso.id}" tabindex="-1" aria-labelledby="exampleModalLabel${curso.id}" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel${curso.id}">${curso.title}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                        <div class="col-md-12">
+                        <div class="card-body">
+                        
+                        <p class="card-text">${curso.description}</p>
+                        <h6 class="card-title">Al finalizar el curso podras:</h6>
+                        <ul id="skills${curso.id}"></ul>
+                        <h6 class="card-title">Unidades:</h6>
+                        <ul id="units${curso.id}"></ul>
+                        <p class="card-text">Duracion: ${curso.duration} semanas</p>
+                        <p class="card-text">Modalidad: ${curso.modality}</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Agregar al Carrito</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                            </div>`;
+    for (const skill of curso.skills) {
+      document.getElementById(
+        `skills${curso.id}`
+      ).innerHTML += `<li>${skill.desciption}</li>`;
+    }
+    for (const unit of curso.units) {
+      document.getElementById(
+        `units${curso.id}`
+      ).innerHTML += `<li>${unit.title}</li>`;
+    }
+  }
 }
-
--- src/js/db.js
+++ src/js/db.js
@@ -1,47 +1,368 @@
 const cursos=[
     {
         id:"1",
-        title:"Titutlo del curso",
+        title:"Aspectos Tecnológicos en Metalurgia y Metálmecanica",
         category:"yoga",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vel corrupti illo nisi, quasi quidem in! Nostrum totam ut iusto itaque necessitatibus pariatur quas magni, sint reiciendis vero nam quo!",
-        img:"https://img.freepik.com/foto-gratis/estudiante-vista-lateral-que-asiste-al-curso_23-2148771467.jpg?w=740&t=st=1647612446~exp=1647613046~hmac=d5417a23ce4223a453c15e31ab44afa168fab9db281b1be01fa7713d43df22b3"
+        description: "En este curso aprenderemos los aspectos tecnológicos asociados a la metalurgia y la metalmecánica hacen referencia a las teorías y técnicas que posibilitan el entendimiento de los procesos de beneficio de los minerales, transformación y caracterización de los productos metálicos para la producción de partes y piezas que son esenciales para la vida cotidiana.",
+        skills:[
+            {
+                id:"1",
+                desciption:"Conocer la evolución, alcance, importancia y términos asociados con la metalurgia."
+            },
+            {
+                id:"2",
+                desciption:"Entender la relevancia de la metalurgia y la metalmecánica en la tecnología e ingeniería."
+            },
+            {
+                id:"3",
+                desciption:"Comprender los fundamentos básicos de la metalurgia química, física y mecánica."
+            },
+            {
+                id:"4",
+                desciption:"Comprender los principios básicos que rigen los procesos de conformado de piezas metálicas."
+            }
+        ],
+        units:[
+            {
+                id:"1",
+                title:"Introducción"
+            },
+            {
+                id:"2",
+                title:"Fundamentos básicos de la Metalurgia y propiedades mecánicas más    relevantes."
+            },
+            {
+                id:"3",
+                title:"Relevancia de la Metalurgia y la metalmecánica en el contexto tecnológico"
+            },
+            {
+                id:"4",
+                title:"Aspectos tecnológicos de la metalmecánica y de los procesos de conformado"
+            }
+        ],
+        duration:2,
+        modality:"On Line",
+        img:"../../public/img/logo.png"
     },
     {
         id:"2",
-        title:"Titutlo del curso",
+        title:"CERTIFICADO EN GESTIÓN DE NEGOCIOS",
         category:"yoga",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vel corrupti illo nisi, quasi quidem in! Nostrum totam ut iusto itaque necessitatibus pariatur quas magni, sint reiciendis vero nam quo!",
-        img:"https://img.freepik.com/foto-gratis/estudiante-vista-lateral-que-asiste-al-curso_23-2148771467.jpg?w=740&t=st=1647612446~exp=1647613046~hmac=d5417a23ce4223a453c15e31ab44afa168fab9db281b1be01fa7713d43df22b3"
+        description: "Los profesionales que conducen empresas y organizaciones necesitan disponer de herramientas útiles que les permitan mejorar su desempeño como gerentes y asegurar la rentabilidad y sustentabilidad de las empresas que conducen. Gracias a este programa podrás desempeñarte con conocimiento práctico sobre la gestión de empresas, atendiendo a su integralidad, pero también comprendiendo la especificidad de las distintas áreas funcionales que la componen, con sentido de responsabilidad social y de sustentabilidad económica.",
+        skills:[
+            {
+                id:"1",
+                desciption:"Compartir procesos, formas de trabajo y casos de uso relevantes."
+            },
+            {
+                id:"2",
+                desciption:"Entender cómo el cerebro procesa la información de manera visual y cómo la podemos aplicar al análisis y comunicación de datos."
+            },
+            {
+                id:"3",
+                desciption:"Definir una serie de buenas prácticas en visualización de datos."
+            },
+            {
+                id:"4",
+                desciption:"Conocer los componentes básicos de Python y los entornos de trabajo"
+            },
+            {
+                id:"5",
+                desciption:"Entender el impacto de la ciencia de datos en las organizaciones y las diferentes fases de una estrategia Data Driven."
+            }
+        ],
+        units:[
+            {
+                id:"1",
+                title:"Data Driven Transformation"
+            },
+            {
+                id:"2",
+                title:"Data Visualization: analítica para la toma de decisiones"
+            },
+            {
+                id:"3",
+                title:"Data Science para la inteligencia del negocio"
+            }
+        ],
+        duration:8,
+        modality:"On Line",
+        img:"../../public/img/logo.png"
     },
     {
         id:"3",
-        title:"Titutlo del curso",
+        title:"CERTIFICACIÓN INT. EN INNOVACIÓN EMPRESARIAL ",
         category:"yoga",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vel corrupti illo nisi, quasi quidem in! Nostrum totam ut iusto itaque necessitatibus pariatur quas magni, sint reiciendis vero nam quo!",
-        img:"https://img.freepik.com/foto-gratis/estudiante-vista-lateral-que-asiste-al-curso_23-2148771467.jpg?w=740&t=st=1647612446~exp=1647613046~hmac=d5417a23ce4223a453c15e31ab44afa168fab9db281b1be01fa7713d43df22b3"
+        description: "El programa busca abordar y caracterizar las nuevas tendencias acerca de la innovación y sus aplicaciones al mundo empresarial con el fin de que los estudiantes del programa puedan diseñar propuestas pertinentes a sus entornos laborales y profesionales.",
+        skills:[
+            {
+                id:"1",
+                desciption:"Tener la capacidad de desarrollar conocimientos y habilidades para realizar los procedimientos y operaciones en su trabajo diario."
+            },
+            {
+                id:"2",
+                desciption:"Desarrollar saberes Valorativos"
+            },
+            {
+                id:"3",
+                desciption:"Mejorar relaciones sociales y convivir en el entorno laboral."
+            }
+        ],
+        units:[
+            {
+                id:"1",
+                title:"Que es la Innovación "
+            },
+            {
+                id:"2",
+                title:"El proceso de Innovación"
+            },
+            {
+                id:"3",
+                title:"Tecnologías Disruptivas "
+            },
+            {
+                id:"4",
+                title:"Como puedo Innovar?"
+            }
+        ],
+        duration:6,
+        modality:"On Line",
+        img:"../../public/img/logo.png"
     },
     {
         id:"4",
-        title:"Titutlo del curso",
+        title:"Dirección Comercial en Mercadotecnia y Publicidad",
         category:"yoga",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vel corrupti illo nisi, quasi quidem in! Nostrum totam ut iusto itaque necessitatibus pariatur quas magni, sint reiciendis vero nam quo!",
-        img:"https://img.freepik.com/vector-gratis/conexion-red-plantilla-curso-negocios-linea_53876-119373.jpg?t=st=1647612440~exp=1647613040~hmac=26910292e5c0dba6481a5f7a0fae5f53d86a24c763100ee3e0543d774c9e22a2&w=740"
+        description: "Desarrollar las competencias necesarias que te permitan desde la Dirección Comercial diseñar y gestionar un plan comercial de ventas para cualquier empresa, de cualquier tamaño y sector, que contribuya a alcanzar sus objetivos de crecimiento y rentabilidad.",
+        skills:[
+            {
+                id:"1",
+                desciption:"Identificar la importancia de la comunicación dentro del proceso de marketing."
+            },
+            {
+                id:"2",
+                desciption:"Conocer como las empresas planifican, implantan y dirigen su fuerza de ventas."
+            },
+            {
+                id:"3",
+                desciption:"Diseñar adecuadamente la estrategia de comunicación de una empresa."
+            },
+            {
+                id:"4",
+                desciption:"Reconocer la importancia de la formación de vendedores."
+            },
+            {
+                id:"5",
+                desciption:"Analizar las nuevas tecnologías disponibles para realizar campañas publicitarias."
+            },
+            {
+                id:"6",
+                desciption:"Evaluar y mesurar los resultados de una campaña publicitaria."
+            }
+        ],
+        units:[
+            {
+                id:"1",
+                title:"Data Driven Transformation"
+            },
+            {
+                id:"2",
+                title:"Data Visualization: analítica para la toma de decisiones"
+            },
+            {
+                id:"3",
+                title:"Data Science para la inteligencia del negocio"
+            },
+            {
+                id:"4",
+                title:"Data Science para la inteligencia del negocio"
+            }
+        ],
+        duration:20,
+        modality:"On Line",
+        img:"../../public/img/logo.png"
     },
     {
         id:"5",
-        title:"Titutlo del curso",
+        title:"CERTIFICADO EN ESTADÍSTICAS PARA LA TOMA DE DECISIONES",
         category:"yoga",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vel corrupti illo nisi, quasi quidem in! Nostrum totam ut iusto itaque necessitatibus pariatur quas magni, sint reiciendis vero nam quo!",
-        img:"https://img.freepik.com/foto-gratis/estudiante-vista-lateral-que-asiste-al-curso_23-2148771467.jpg?w=740&t=st=1647612446~exp=1647613046~hmac=d5417a23ce4223a453c15e31ab44afa168fab9db281b1be01fa7713d43df22b3"
+        description: "Pequeños y medianos empresarios, profesionales con cargos gerenciales dentro de corporaciones, emprendedores, profesionales relacionados a equipos de marketing, personas que esté interesada en el aprendizaje de la gestión y análisis integral de datos.",
+        skills:[
+            {
+                id:"1",
+                desciption:"Tener la habilidad para obtener, resumir, interpretar y mostrar datos y parámetros estadísticos. Comenzando por los conceptos básicos se podrá trabajar con datos y poder manejar herramientas computacionales y matemáticas para su análisis y procesamiento"
+            },
+            {
+                id:"2",
+                desciption:"Adquirir el conocimiento para correlacionar variables."
+            },
+            {
+                id:"3",
+                desciption:"Capacidad de obtener las bases para aplicar un criterio lógico a nivel decisorio y de evaluación de distintos escenarios posibles frente a la probabilidad de ocurrencia de eventos."
+            },
+            {
+                id:"4",
+                desciption:"Manejar la información estadística de poblaciones teniendo la posibilidad de discriminar en las distintas situaciones posibles realizando un análisis racional y ético de las observaciones."
+            }
+        ],
+        units:[
+            {
+                id:"1",
+                title:"DISTRIBUCIONES DE PROBABILIDADES"
+            },
+            {
+                id:"2",
+                title:"PREDECIR, ELEGIR Y ARRIESGAR ESTADÍSTICAMENTE. PRUEBA DE HUIPÓTESIS"
+            },
+            {
+                id:"3",
+                title:"FUNDAMENTOS DE ESTADÍSTICA DESCRIPTIVA"
+            },
+            {
+                id:"4",
+                title:"ASOCIACIÓN ENTRE VARIABLES: CONTINGENCIA, REGRESIÓN Y CORRELACIÓN"
+            }
+        ],
+        duration:12,
+        modality:"On Line",
+        img:"../../public/img/logo.png"
     },
     {
         id:"6",
-        title:"Titutlo del curso",
+        title:"Curso de logística",
         category:"yoga",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vel corrupti illo nisi, quasi quidem in! Nostrum totam ut iusto itaque necessitatibus pariatur quas magni, sint reiciendis vero nam quo!",
-        img:"https://img.freepik.com/foto-gratis/estudiante-vista-lateral-que-asiste-al-curso_23-2148771467.jpg?w=740&t=st=1647612446~exp=1647613046~hmac=d5417a23ce4223a453c15e31ab44afa168fab9db281b1be01fa7713d43df22b3"
+        description: "En este curso de logística de distribución aprenderás a llevar la trazabilidad de la gestión de la cadena de suministro de tus productos, desde su preparación hasta su salida. Asimismo, aprenderás los conceptos relacionados con la gestión de pedidos, gestión de inventarios y el embalaje de productos.",
+        skills:[
+            {
+                id:"1",
+                desciption:"Comprender los fundamentos de la logística de distribución y su aplicación en las organizaciones."
+            },
+            {
+                id:"2",
+                desciption:"Aplicar técnicas y herramientas para la preparación de pedidos."
+            },
+            {
+                id:"3",
+                desciption:"Aplicar modelos y métricas de inventario adecuado para el almacenamiento de productos en bodegas"
+            },
+            {
+                id:"4",
+                desciption:"Aplicar técnicas y herramientas para el diseño del proceso de embalaje de productos."
+            }
+        ],
+        units:[
+            {
+                id:"1",
+                title:"Fundamentos de la Logística de distribución y su aplicación en las organizaciones."
+            },
+            {
+                id:"2",
+                title:"Aplicar técnicas y herramientas para el diseño del proceso de embalaje de productos."
+            },
+            {
+                id:"3",
+                title:"Gestion de Pedidos"
+            },
+            {
+                id:"4",
+                title:"Proceso de embalaje de productos"
+            }
+        ],
+        duration:4,
+        modality:"On Line",
+        img:"../../public/img/logo.png"
+    },
+    {
+        id:"7",
+        title:"Competencias Digitales. Conceptos y Herramientas Básicas",
+        category:"yoga",
+        description: "Los continuos cambios tecnológicos, sobre todo en aquellos aspectos vinculados a las tecnologías de la información y la comunicación (TIC) hacen que las personas tengan la necesidad de actualizarse de forma continua para que sus conocimientos no queden obsoletos. En este contexto, para las empresas se convierte en algo imprescindible disponer de profesionales que tengan las competencias necesarias para ejercer con éxito las actividades que requieren en su lugar de trabajo.",
+        skills:[
+            {
+                id:"1",
+                desciption:"Ver el desarrollo continuo de las personas en aquellas competencias vinculadas a las tecnologías de la información y la comunicación."
+            },
+            {
+                id:"2",
+                desciption:"Facilitar la inserción laboral y la renovación de las competencias tecnológicas de los estudiantes y de los profesionales."
+            }
+        ],
+        units:[
+            {
+                id:"1",
+                title:"Conceptos Básicos"
+            },
+            {
+                id:"2",
+                title:"Uso del sistema operativo."
+            },
+            {
+                id:"3",
+                title:"Conocimientos en Internet."
+            },
+            {
+                id:"4",
+                title:"Seguridad Informática"
+            }
+        ],
+        duration:4,
+        modality:"On Line",
+        img:"../../public/img/logo.png"    
+    },
+    {
+        id:"8",
+        title:"CERTIFICADO EN GESTIÓN DE NEGOCIOS",
+        category:"yoga",
+        description: "Los profesionales que conducen empresas y organizaciones necesitan disponer de herramientas útiles que les permitan mejorar su desempeño como gerentes y asegurar la rentabilidad y sustentabilidad de las empresas que conducen. Gracias a este programa podrás desempeñarte con conocimiento práctico sobre la gestión de empresas, atendiendo a su integralidad, pero también comprendiendo la especificidad de las distintas áreas funcionales que la componen, con sentido de responsabilidad social y de sustentabilidad económica.",
+        skills:[
+            {
+                id:"1",
+                desciption:"Capacidad para liderar grupos humanos asignando roles y fijando objetivos desafiantes manteniendo comunicaciones abiertas que minimicen los conflictos laborales y potencien la capacidad de gestión estratégica y la toma de decisiones."
+            },
+            {
+                id:"2",
+                desciption:"Habilidad para interpretar los cambios que se producen en el mercado y proponer acciones de marketing acordes a las necesidades de los consumidores."
+            },
+            {
+                id:"3",
+                desciption:"Habilidad de seleccionar e implementar estrategias organizacionales en el ámbito de la gestión empresarial."
+            },
+            {
+                id:"4",
+                desciption:"Capacidad para modelar una oportunidad de negocio"
+            }
+        ],
+        units:[
+            {
+                id:"1",
+                title:"GESTION ADMINISTRATIVA"
+            },
+            {
+                id:"2",
+                title:"GESTION COMERCIAL"
+            },
+            {
+                id:"3",
+                title:"LIDERAZGO Y EQUIPOS DE ALTO RENDIMIENTO"
+            },
+            {
+                id:"4",
+                title:"RECURSOS HUMANOS"
+            },
+            {
+                id:"5",
+                title:"OPERACIONES Y PRODUCCION"
+            },
+            {
+                id:"6",
+                title:"GESTION FINANCIERA"
+            } 
+        ],
+        duration:16,
+        modality:"On Line",
+        img:"../../public/img/logo.png"    
     }
-
 ];
 
 export default cursos;
